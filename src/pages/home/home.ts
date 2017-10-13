f6b725e5e8e913d2067952df90a3eecf704c1c9c
Muhammed Essa Hameed <muhammed@fedora.muhammed>
2017-10-13 03:28:18 +0300

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner,BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,46 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  data={ };
+  encodemyData:string;
+encodedData:{};
+
+  option:BarcodeScannerOptions ;
+  constructor(public navCtrl: NavController,public barcodeScanner:BarcodeScanner) {
 
   }
+
+  scan(){
+
+    this.option = {
+
+      prompt: "Please scan your code"
+    }
+    this.barcodeScanner.scan(this.option).then((barcodeData) => {
+      // Success! Barcode data is here
+      console.log(barcodeData);
+      this.data = barcodeData;
+
+
+     }, (err) => {
+         // An error occurred
+         console.log(err);
+     });
+
+  }
+
+
+  encodeData(){
+this.barcodeScanner.encode(this.barcodeScanner.Encode.TEXT_TYPE,this.encodemyData).then((res)=>{
+console.log(res)
+this.encodedData = res;
+}, (err) => {
+  // An error occurred
+  console.log(err);
+})
+  }
+
+
+
 
 }
